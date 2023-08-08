c991182a1ddb832c2c443cd7027aa52620a00223
Max Gomes <maxgomes92@live.com>
2023-08-08 22:46:44 +0200

    feat: improve home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,11 @@
-import { Button, Container } from "@mui/material";
-import React from "react";
+import { Button, Container, Typography } from "@mui/material";
+import React, { useState } from "react";
 import logo from "./icon.png";
 import Separator from "../../components/Separator";
 import useApi from "../../helpers/useApi";
-import "./index.css";
 import { PATH } from "../../utils/constants";
 import { Navigate } from "react-router";
+import "./index.css";
 
 const imageStyle = { height: 200, width: 'auto' }
 
@@ -35,30 +35,59 @@ const sideStyle = {
 
 export default function Home() {
   const { authorize, loggedIn } = useApi();
+  const [hasClickedLogin, setHasClickedLogin] = useState(false)
+
+  const onLoginClick = () => {
+    setHasClickedLogin(true)
+    authorize()
+  }
 
   return (
-    <Container style={wrapperStyle}>
+    <Container style={wrapperStyle} maxWidth="md">
+      {hasClickedLogin && loggedIn && (
+        <Navigate to={PATH.app} replace={true} />
+      )}
+
       <div style={contentStyle}>
         <div style={sideStyle}>
-          <h1 style={{ textAlign: 'center' }}>
-            Convert your mp3 files to a Spotify playlist with ease!
-          </h1>
+          <Typography>MP3 to Spotify</Typography>
+
+          <Separator height={50} />
+
+          <div style={{ flexGrow: 1 }}>
+            <div>
+              <Typography variant="h4" style={{ fontWeight: 'bolder' }}>
+                Create Spotify playlists from your mp3 files with a click!
+              </Typography>
+
+              <Separator height={25} />
+
+              <Typography variant="body1">
+                Get insights into your music listening habits with top artists and tracks. Discover new music, create playlists and get monthly reports.
+              </Typography>
+
+              <Separator height={25} />
+
+              <div style={{ textAlign: 'center' }}>
+                <Button variant="contained" onClick={onLoginClick} style={{ color: 'white', backgroundColor: '#20c933', textTransform: 'none' }}>
+                  <img src={logo} alt="spotify-logo" />
+                  <Separator width={10} />
+                  <b>Sign-in with Spotify</b>
+                </Button>
+              </div>
+            </div>
+          </div>
 
           <div>
-            {loggedIn ? (
-              <Navigate to={PATH.app} replace={true} />
-            ) : (
-              <Button variant="contained" color="success" onClick={authorize}>
-                <img src={logo} alt="spotify-logo" />
-                <Separator width={10} />
-                Login with Spotify
-              </Button>
-            )}
+            <Typography variant="body2" fontSize={10} textAlign="center">
+              <b>Spotify to Playlist</b> use Spotify Web API to perform but application works as client side only and your
+              Spotify data is not stored to any server.
+            </Typography>
           </div>
         </div>
 
 
-        <div style={{ ...sideStyle, background: 'lightgray', alignItems: 'center' }}>
+        <div style={{ ...sideStyle, alignItems: 'center' }} className="gradient-bg">
           <div>
             <img src="/spotify-create-playlist/folder-mp3-list.png" style={imageStyle} />
           </div>
@@ -68,7 +97,7 @@ export default function Home() {
           </div>
 
           <div>
-            <img src="/spotify-create-playlist/app-screenshot.jpg" style={imageStyle} />
+            <img src="/spotify-create-playlist/created-spotify-playlist.png" style={imageStyle} />
           </div>
         </div>
       </div>
