857a81b5dd8eff19323c0abeb260a6a993d3f80e
Max Gomes <maxgomes92@live.com>
2023-08-14 03:08:50 +0200

    feat: add gtm

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
 import Sentry from "./utils/Sentry";
+import { GTMProvider } from '@elgorditosalsero/react-gtm-hook'
 import "./index.css";
 
 Sentry.initialize()
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const gtmParams = { id: 'G-HHD4S99HSL' }
+
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <GTMProvider state={gtmParams}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </GTMProvider>
   </React.StrictMode>
 );
