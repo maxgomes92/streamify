86982dd1ccf5eb081c7de96d7c71d53d11179609
Max Gomes <maxgomes92@live.com>
2023-08-14 03:00:23 +0200

    feat: add gtm

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
 import Sentry from "./utils/Sentry";
+import TagManager from 'react-gtm-module'
 import "./index.css";
 
 Sentry.initialize()
+
+const tagManagerArgs = {
+  gtmId: 'G-HHD4S99HSL'
+}
+
+TagManager.initialize(tagManagerArgs)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
